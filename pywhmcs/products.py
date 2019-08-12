90d41e7f6d8ac33afa0a28bc6cf1da8836a61154
Zane Mingee <z.mingee@gmail.com>
2019-08-12 19:57:39 -0400

    refactor(products): unified internal API

--- pywhmcs/products.py
+++ pywhmcs/products.py
@@ -1,39 +1,95 @@
+from typing import Any, Dict, List, Optional, Union
+import dataclasses
 
 from pywhmcs import base
 
 
+@dataclasses.dataclass
+class Product(base.BaseResource):
+    id: int
+    configoptions: List[Dict[str, Any]]
+    customfields: List[str]
+    description: Optional[str]
+    group_id: int
+    module: str
+    name: str
+    paytype: str
+    pricing: Dict[str, Dict[str, str]]
+    type: str
+
+
 class ProductsBridge(base.BaseBridge):
 
-    def get_products(self, product_id: int = None, group_id: int = None,
-                     module: str = None) -> dict:
+    def get(self, resource: Union[str, int]) -> Product:
         """
-        Get products matching given criteria.
+        Get a product by ID
 
-        :param int product_id: Limit search to specific product ID
-        :param int group_id: Limit search to a specific product group ID
-        :param str module: Limit search to products utilising a specific module
+        :param product: ID of product to get
+        :return: Product
+        :rtype: :class:`Product`
+        :raises: :class:`pywhmcs.exceptions.ProductNotFound`
+        """
+
+        response = self.client.send_request(
+            'getproducts',
+            params={'pid': int(resource)}
+        )
+
+        whmcs_product = response['products']['product'][0]
+
+        product = Product(
+            self,
+            id=int(whmcs_product['pid']),
+            configoptions=whmcs_product['configoptions']['configoption'],
+            customfields=whmcs_product['customfields']['customfield'],
+            description=whmcs_product['description'] or None,
+            group_id=int(whmcs_product['gid']),
+            module=whmcs_product['module'],
+            name=whmcs_product['name'],
+            paytype=whmcs_product['paytype'],
+            pricing=whmcs_product['pricing'],
+            type=whmcs_product['type']
+        )
+
+        return product
+
+    def list(self, detailed=True, marker=None, limit=None, **kwargs) -> List[Product]:
+        """
+        List and filter products.
+
+        :param str product_id: Product ID to filter by
+        :param str group_id: Group ID to filter by
+        :param str module: Module to filter by
         :return: Products matching given criteria
-        :rtype: dict
+        :rtype: List[:class:`Product`]
         """
 
         params = {
-            "pid": product_id,
-            "gid": group_id,
-            "module": module
-        }
-        params = {k: v for k, v in params.items() if v is not None}
-
-        response = self.client.send_request("GetProducts", params)
-
-        if not response["numreturned"]:
-            products = []
-        else:
-            products = response["products"]["product"]
-
-        return {
-            "total": int(response["totalresults"]),
-            "products": products,
-            "start_number": int(response["startnumber"])
+            key: value for (key, value)
+            in {
+                'pid': kwargs.get('product_id'),
+                'gid': kwargs.get('group_id'),
+                'module': kwargs.get('module')
+            }.items() if value is not None
         }
 
+        response = self.client.send_request('getproducts', params)
 
+        matches = []
+        for whmcs_product in response['products']['product']:
+            product = Product(
+                self,
+                id=int(whmcs_product['pid']),
+                configoptions=whmcs_product['configoptions']['configoption'],
+                customfields=whmcs_product['customfields']['customfield'],
+                description=whmcs_product['description'] or None,
+                group_id=int(whmcs_product['gid']),
+                module=whmcs_product['module'],
+                name=whmcs_product['name'],
+                paytype=whmcs_product['paytype'],
+                pricing=whmcs_product['pricing'],
+                type=whmcs_product['type']
+            )
+            matches.append(product)
+
+        return matches
