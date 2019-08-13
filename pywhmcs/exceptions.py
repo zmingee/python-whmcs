948f5dd7ef5dae3c57ac01c639d2da9d7d269be9
Zane Mingee <z.mingee@gmail.com>
2019-08-13 19:00:47 -0400

    fix(exceptions): add missing exceptions

--- pywhmcs/exceptions.py
+++ pywhmcs/exceptions.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import List, Optional, Union
 
 class WHMCSException(Exception):
     """Base exception class for all internal exceptions."""
     message: str
-    whmcs_message: Optional[str] = None
+    whmcs_message: Optional[Union[str, List[str]]] = None
 
     def __init__(self, message=None, action=None, response=None):
         super().__init__(message or self.__class__.message)
@@ -21,12 +21,46 @@ class ResourceNotFound(Exception):
 
 class CommandNotFound(WHMCSException):
     message = 'Command not found'
-    whmcs_message = 'command not found'
+    whmcs_message = message.lower()
 
 
 class ClientNotFound(WHMCSException, ResourceNotFound):
     message = 'Client not found'
-    whmcs_message = 'client not found'
+    whmcs_message = message.lower()
+
+
+class UserAlreadyExists(WHMCSException):
+    """Raised when account already exists with specified email address."""
+    message = 'A user already exists with that email address'
+    whmcs_message = message.lower()
+
+
+class OrderNotFound(WHMCSException, ResourceNotFound):
+    message = 'Order ID not found'
+    whmcs_message = [
+        message.lower(),
+        'Order ID not found or Status not Pending'.lower()
+    ]
+
+
+class PaymentFailed(WHMCSException):
+    message = 'Payment attempt failed'
+    whmcs_message = message.lower()
+
+
+class InvalidEmailOrPassword(WHMCSException):
+    message = 'Email or Password Invalid'
+    whmcs_message = message.lower()
+
+
+class InvoiceNotFound(WHMCSException, ResourceNotFound):
+    message = 'Invoice ID Not Found'
+    whmcs_message = message.lower()
+
+
+class TicketNotFound(WHMCSException, ResourceNotFound):
+    message = 'Ticket ID not found'
+    whmcs_message = message.lower()
 
 
 ###
@@ -35,11 +69,6 @@ class ClientNotFound(WHMCSException, ResourceNotFound):
 class RestrictedIPError(WHMCSException):
     """Raised when attempting to call API from a non-whitelisted IP Address."""
     message = 'Source IP is not white-listed'
-
-
-class UserAlreadyExists(WHMCSException):
-    """Raised when account already exists with specified email address."""
-    message = 'A user already exists with that email address'
 
 
 class AuthenticationFailed(WHMCSException):
@@ -86,7 +115,7 @@ class ClientIDNotFound(WHMCSException):
 
 
 _error_classes = WHMCSException.__subclasses__()
-_code_map = dict((c.whmcs_message, c) for c in _error_classes if c.whmcs_message)
+_code_map = tuple((c.whmcs_message, c) for c in _error_classes if c.whmcs_message)
 
 
 def from_response(response, action):
@@ -99,9 +128,11 @@ def from_response(response, action):
 
     whmcs_message = content['message'].lower()
 
-    cls = _code_map.get(whmcs_message)
-    if cls:
-        cls = cls(action=action, response=response)
+    for (msg, exc) in _code_map:
+        if (isinstance(msg, list) and whmcs_message in msg
+                or whmcs_message == msg):
+            cls = exc(action=action, response=response)
+            break
     else:
         cls = UnknownError(content['message'], action=action, response=response)
 
